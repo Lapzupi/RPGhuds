318b188689d6cda5c43a39869bf614759d52dd77
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-29 12:17:11 +0300

    Now using configurate for settings

--- src/main/java/dev/lone/rpghuds/core/config/Settings.java
+++ src/main/java/dev/lone/rpghuds/core/config/Settings.java
@@ -2,11 +2,13 @@ package dev.lone.rpghuds.core.config;
 
 import com.github.sarhatabaot.kraken.core.config.Transformation;
 import com.github.sarhatabaot.kraken.core.config.YamlConfigurateFile;
+import com.google.common.collect.ImmutableMap;
 import dev.lone.rpghuds.Main;
 import dev.lone.rpghuds.utils.Utilz;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.ConfigurationNode;
@@ -16,6 +18,7 @@ import org.spongepowered.configurate.serialize.TypeSerializer;
 import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Settings extends YamlConfigurateFile<Main> {
@@ -28,6 +31,9 @@ public class Settings extends YamlConfigurateFile<Main> {
     private String msgDestinationRemoved;
 
     private List<HudConfig> hudList;
+
+    private ImmutableMap<String, String> moneyType;
+    private ImmutableMap<String, String> compassType;
 
     public Settings(@NotNull final Main plugin) throws ConfigurateException {
         super(plugin, "", "config.yml", "");
@@ -46,12 +52,25 @@ public class Settings extends YamlConfigurateFile<Main> {
         this.msgWrongUsage = Utilz.color(langNode.node("wrong_usage").getString("&cWrong command usage."));
         this.msgDestinationSet = Utilz.color(langNode.node("destination_set").getString("&aDestination set."));
         this.msgDestinationRemoved = Utilz.color(langNode.node("destination_removed").getString("&7Destination removed."));
+
+        CommentedConfigurationNode typesNode = rootNode.node("types");
+        this.moneyType = getMapFromConfiguration(typesNode.node("money").childrenMap());
+        this.compassType = getMapFromConfiguration(typesNode.node("compass").childrenMap());
+    }
+
+    private ImmutableMap<String, String> getMapFromConfiguration(Map<Object, CommentedConfigurationNode> map) {
+        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+        for (Map.Entry<Object, CommentedConfigurationNode> entry : map.entrySet()) {
+            builder.put(entry.getValue().key().toString(), entry.getValue().getString());
+        }
+        return builder.build();
     }
 
     @Override
     protected void builderOptions() {
         loaderBuilder.defaultOptions(options -> options.serializers(builder ->
-                builder.registerExact(HudConfig.class, new HudConfigTypeSerializer())));
+                builder.registerExact(HudType.class, new HudTypeSerializer())
+                .registerExact(HudConfig.class, new HudConfigTypeSerializer())));
         //nothing
     }
 
@@ -94,7 +113,7 @@ public class Settings extends YamlConfigurateFile<Main> {
 
 
     public static class HudConfigTypeSerializer implements TypeSerializer<HudConfig> {
-        private static final String TYPE = "type";
+        private static final String TYPE = "hud-type";
         private static final String ENABLED = "enabled";
         private static final String OFFSET = "offset";
         private static final String PAPI_PLACEHOLDER = "papi_placeholder";
@@ -102,7 +121,7 @@ public class Settings extends YamlConfigurateFile<Main> {
 
         @Override
         public HudConfig deserialize(final Type type, final ConfigurationNode node) throws SerializationException {
-            final HudType hudType = node.node(TYPE).get(HudType.class);
+            final HudType hudType = node.node(TYPE).get(HudType.class, HudType.MONEY);
             final boolean enabled = node.node(ENABLED).getBoolean(true);
             final int offset = node.node(OFFSET).getInt();
             final String papiPlaceholder = node.node(PAPI_PLACEHOLDER).getString();
@@ -110,14 +129,14 @@ public class Settings extends YamlConfigurateFile<Main> {
             final ConfigurationNode settings = node.node("settings");
             final String namespacedId = settings.node(NAMESPACED_ID).getString();
             final List<String> worlds = node.node("worlds").getList(String.class);
+
             if (hudType == HudType.MONEY) {
                 final String icon = settings.node("icon").getString();
                 return new MoneyHudConfig(hudType, enabled, offset, papiPlaceholder, worlds, namespacedId, icon);
             }
 
-            final String prefix= settings.node("prefix").getString();
+            final String prefix = settings.node("prefix").getString();
             return new CompassHudConfig(hudType, enabled, offset, papiPlaceholder, worlds, namespacedId, prefix);
-
         }
 
         @Override
@@ -126,7 +145,27 @@ public class Settings extends YamlConfigurateFile<Main> {
         }
     }
 
+    public static class HudTypeSerializer implements TypeSerializer<HudType> {
+        @Override
+        public HudType deserialize(final Type type, final ConfigurationNode node) throws SerializationException {
+            return HudType.valueOf(node.getString("money").toUpperCase());
+        }
+
+        @Override
+        public void serialize(final Type type, @Nullable final HudType obj, final ConfigurationNode node) throws SerializationException {
+            //we don't use this
+        }
+    }
+
     public List<HudConfig> getHudList() {
         return hudList;
+    }
+
+    public ImmutableMap<String, String> getMoneyType() {
+        return moneyType;
+    }
+
+    public ImmutableMap<String, String> getCompassType() {
+        return compassType;
     }
 }
