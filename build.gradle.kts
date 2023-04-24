2a946aa6a14647bacea574979ba6cf5f3a26cd49
Omer Oreg <sarhatabaot1@gmail.com>
2023-04-24 21:50:46 +0300

    Moved to gradle (kotlin)

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("java")
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
+    id("com.github.johnrengelman.shadow") version "8.1.0"
+}
+
+group = "dev.lone"
+version = "1.3.1"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.aikar.co/content/groups/aikar/")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.codemc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+    compileOnly(libs.vault.api)
+    compileOnly(libs.placeholder.api)
+    compileOnly(libs.itemsadder.api)
+
+    implementation(libs.commands.paper)
+    implementation(libs.lapzupi.config)
+
+    library(libs.configurate.yaml)
+    library(libs.commons.io)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    
+    shadowJar {
+        minimize()
+        
+        dependencies {
+            exclude(dependency("net.kyori:.*:.*"))
+        }
+        
+        archiveFileName.set("LapzupiHuds-${project.version}.jar")
+        archiveClassifier.set("shadow")
+    }
+}
+
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "lapzupi-huds"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("paper-api", "io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+            library("itemsadder-api", "com.github.LoneDev6:api-itemsadder:3.4.1-r4")
+            library("vault-api", "com.github.MilkBowl:VaultAPI:1.7.1")
+            library("placeholder-api", "me.clip:placeholderapi:2.11.3")
+            library("commands-paper", "co.aikar:acf-paper:0.5.1-SNAPSHOT")
+            library("commons-io", "commons-io:commons-io:2.11.0")
+            library("lapzupi-config","com.github.Lapzupi:LapzupiConfig:1.1.0")
+            library("configurate-yaml", "org.spongepowered:configurate-yaml:4.1.2")
+        }
+    }
+}
